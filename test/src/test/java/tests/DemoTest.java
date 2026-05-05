df7fd9fbf00ec17402d9e9eed2a411bb5da9ee62
Levente Fodor <leventefodor@gmail.com>
2026-05-05 23:06:34 +0200

    Added custom options to webdriver configuration.

--- test/src/test/java/tests/DemoTest.java
+++ test/src/test/java/tests/DemoTest.java
@@ -2,44 +2,17 @@ package tests;
 
 import org.testng.Assert;
 import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.*;
-import org.openqa.selenium.firefox.*;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.URL;
 import java.net.MalformedURLException;
 
 import pages.MainPage;
-import utils.ConfigReader;
 
 public class DemoTest extends TestBase {
 
     @BeforeMethod
     public void setup() throws MalformedURLException {
-
-        Capabilities caps;
-        boolean headless = Boolean.parseBoolean(ConfigReader.get("headless"));
-
-        switch (ConfigReader.get("browser")) {
-            case "chrome":
-                ChromeOptions chromeOptions = new ChromeOptions();
-                if (headless) {
-                    chromeOptions.addArguments("--headless=new");
-                }
-                caps = chromeOptions;
-                break;
-
-            case "firefox":
-            default:
-                FirefoxOptions firefoxOptions = new FirefoxOptions();
-                if (headless) {
-                    firefoxOptions.addArguments("-headless");
-                }
-                caps = firefoxOptions;
-                break;
-        }
-
-        this.driver = new RemoteWebDriver(new URL("http://selenium-hub:4444"), caps);
+        this.driver = new RemoteWebDriver(new URL("http://selenium-hub:4444"), createCapabilities());
         this.driver.manage().window().maximize();
     }
 
--- test/src/test/java/tests/TestBase.java
+++ test/src/test/java/tests/TestBase.java
@@ -1,15 +1,54 @@
 package tests;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Listeners;
 
+import utils.ConfigReader;
 import utils.ScreenshotListener;
 
 @Listeners(ScreenshotListener.class)
 public class TestBase {
     protected WebDriver driver;
 
+    protected Capabilities createCapabilities() {
+
+        boolean headless = Boolean.parseBoolean(ConfigReader.get("headless"));
+        String browser = ConfigReader.get("browser");
+
+        switch (browser) {
+
+            case "chrome":
+                ChromeOptions chromeOptions = new ChromeOptions();
+
+                chromeOptions.addArguments("--disable-notifications");
+                chromeOptions.addArguments("--window-size=1920,1080");
+
+                if (headless) {
+                    chromeOptions.addArguments("--headless=new");
+                }
+
+                return chromeOptions;
+
+            case "firefox":
+            default:
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+
+                firefoxOptions.addArguments("--width=1920");
+                firefoxOptions.addArguments("--height=1080");
+                firefoxOptions.addPreference("dom.webnotifications.enabled", false);
+
+                if (headless) {
+                    firefoxOptions.addArguments("-headless");
+                }
+
+                return firefoxOptions;
+        }
+    }
+    
     @AfterMethod
     public void close() {
         if (driver != null) {
